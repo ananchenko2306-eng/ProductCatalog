fcaf863eba492d1b49d3e8bba857244cdf216ac3
Lena <ananchenko2306@gmail.com>
2025-11-19 22:03:55 +0200

    added due date

--- src/main/java/com/example/ProductCatalog/Product.java
+++ src/main/java/com/example/ProductCatalog/Product.java
@@ -1,33 +1,62 @@
 package com.example.ProductCatalog;
 
+import java.time.LocalDate;
+
+@SuppressWarnings("unused")
 public class Product {
     private long id;
     private String name;
     private double price;
-    // Конструктори, гетери та сетери
-    public Product() {}
+    private LocalDate dueDate;
+
+    public Product() {
+    }
+
     public Product(long id, String name, double price) {
         this.id = id;
         this.name = name;
         this.price = price;
     }
 
+    public Product(long id, String name, double price, LocalDate dueDate) {
+        this(id, name, price);
+        this.dueDate = dueDate;
+    }
+
     public long getId() {
         return id;
     }
+
     public void setId(long id) {
         this.id = id;
     }
+
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public double getPrice() {
         return price;
     }
+
     public void setPrice(double price) {
         this.price = price;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(LocalDate dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{id=" + id + ", name='" + name + "', price=" + price + ", dueDate=" + dueDate + "}";
     }
 }
